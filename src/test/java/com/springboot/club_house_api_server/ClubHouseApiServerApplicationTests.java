76cbdbab96e04cf2d6df614eeb5dab669e415f39
Jun Young <aa2030822@naver.com>
2025-01-24 15:20:25 +0900

    🎉 API Server 초기 세팅 , 프로젝트 시작~~

---
+++ src/test/java/com/springboot/club_house_api_server/ClubHouseApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.club_house_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubHouseApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
