0b6515de2a27828ae8bd525778cd4ee47c2ddcf0
Jun Young <aa2030822@naver.com>
2025-01-31 19:48:20 +0900

    ✨ Jwt accessToken 재발급 기능 구현 완료

--- src/main/java/com/springboot/club_house_api_server/user/controller/UserController.java
+++ src/main/java/com/springboot/club_house_api_server/user/controller/UserController.java
@@ -6,25 +6,32 @@ import com.springboot.club_house_api_server.user.dto.LoginResponseDto;
 import com.springboot.club_house_api_server.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/user")
 public class UserController {
     private final UserService userService;
+    //회원가입 엔드포인트
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody JoinRequestDto request){
         userService.join(request);
         return ResponseEntity.ok("회원가입 성공");
     }
-
+    //로그인 엔드포인트
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request){
         LoginResponseDto r= userService.login(request);
         return ResponseEntity.ok(r);
+    }
+    //Access Token 재발급 엔드포인트
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDto> refresh(@RequestHeader("Authorization") String refreshToken){
+        if(refreshToken!=null || !refreshToken.startsWith("Bearer ")){
+            refreshToken = refreshToken.substring(7);
+        }
+        LoginResponseDto response = userService.refreshToken(refreshToken);
+        return ResponseEntity.ok(response);
     }
 }
