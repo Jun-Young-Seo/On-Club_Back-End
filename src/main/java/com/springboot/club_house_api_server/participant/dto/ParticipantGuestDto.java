611745ea7230183b77ccef1d5f89c453e992404b
seojunyoung <aa2030822@naver.com>
2025-05-21 17:13:56 +0900

    🎨 Participant Guest API 반환값 멤버십과 동일하게 통일

---
+++ src/main/java/com/springboot/club_house_api_server/participant/dto/ParticipantGuestDto.java
@@ -0,0 +1,24 @@
+package com.springboot.club_house_api_server.participant.dto;
+
+import com.springboot.club_house_api_server.user.entity.UserEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class ParticipantGuestDto {
+    private long userId;
+    private String userName;
+    private UserEntity.Gender gender;
+    private LocalDateTime lastGamedAt;
+    private int career;
+    private int gameCount;
+    private LocalDate birthDate;
+    private String region;
+
+}
