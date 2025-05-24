77864fde5601820654e9b12a59bdeff51d12d749
seojunyoung <aa2030822@naver.com>
2025-05-24 16:33:35 +0900

    ⬆️ Secrets로 APNs 관련 의존성 설정

--- src/main/java/com/springboot/club_house_api_server/apn/config/APNsConfig.java
+++ src/main/java/com/springboot/club_house_api_server/apn/config/APNsConfig.java
@@ -3,10 +3,13 @@ package com.springboot.club_house_api_server.apn.config;
 import com.eatthepath.pushy.apns.ApnsClient;
 import com.eatthepath.pushy.apns.ApnsClientBuilder;
 import com.eatthepath.pushy.apns.auth.ApnsSigningKey;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,16 +19,24 @@ import java.security.NoSuchAlgorithmException;
 @Configuration
 public class APNsConfig {
 
-    private final String teamId =  "6VF8H2JP9M";
-    private final String keyId = "ZXXYLTD332";
+    @Value("${apn.bundle.id}")
+    private String bundleId;
+    @Value("${apn.team.id}")
+    private String teamId;
+    @Value("${apn.key.id}")
+    private String keyId ;
+    @Value("${apn.key}")
+    private String key ;
 
     @Bean
     public ApnsClient apnsClient() throws IOException, NoSuchAlgorithmException, InvalidKeyException {
-        return new ApnsClientBuilder()
-                .setApnsServer(ApnsClientBuilder.DEVELOPMENT_APNS_HOST)
-                .setSigningKey(
-                        ApnsSigningKey.loadFromInputStream(new FileInputStream(""), teamId, keyId)
-                )
-                .build();
+        String normalizedPem = key.replace("\\n", "\n");
+//        System.out.println(normalizedPem);
+        try (ByteArrayInputStream keyStream = new ByteArrayInputStream(normalizedPem.getBytes())) {
+            return new ApnsClientBuilder()
+                    .setApnsServer(ApnsClientBuilder.DEVELOPMENT_APNS_HOST)
+                    .setSigningKey(ApnsSigningKey.loadFromInputStream(keyStream, teamId, keyId))
+                    .build();
+        }
     }
 }
